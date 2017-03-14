36f3396dca1160d1d2a41910f6ebeb9ea29167a0
Edward <you@email.com>
2017-03-14 11:23:54 -0700

    added general template

---
+++ themes/yoga_outreach/page-templates/general-template.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: general-template*/ ?>
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+this is general template
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
