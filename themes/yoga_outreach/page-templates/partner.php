86c620a331199964a8b07943932fca286d024435
Montana Sawyer <montanarsawyer@gmail.com>
2017-03-16 17:03:46 -0700

    added mixins

--- themes/yoga_outreach/page-templates/partner.php
+++ themes/yoga_outreach/page-templates/partner.php
@@ -12,7 +12,7 @@ get_header(); ?>
             <header class="entry-header custom-hero-image">
                 <h1 class="header-title"><?php the_title(); ?></h1>
                 <p>We partners with yoga instructors, community organizations, social service agencies, and prisons to provide trauma-informed and strengths-based yoga programming for our community.</p>
-                <div class="button-wrap">
+                <div>
                     <button class="button-holder">What We Provide</button>
                     <button class="button-holder">A Facility Perspective</button>
                     <button class="button-holder">Receive your Program</button>
@@ -24,13 +24,13 @@ get_header(); ?>
                 <p><?php echo CFS()->get( 'what_we_provide' ); ?></p>
                 <h3>Apply for Partnership</h3>
                 <p>We process new applications on a monthly basis, and will be in touch with you about your application as soon as we can.</p>
-                <div class="button-case-holder"><button class="button-holder">Apply Now</button></div>  
+                <div class="button-wrap"><button class="button-holder">Apply Now</button></div>  
             </section>
             <section class="container-two">
                 <h2 class="h2-holder">A Facility Perspective</h2>
                 <img src="<?php echo CFS()->get( 'facility_video' ); ?>" />
             </section>
-            <section class="volunteer-community partner-facility">
+            <section class="container-one partner-facility">
                 <h2 class="h2-holder">Receive yoga for Outreach Programming</h2>
                 <div class = "interested-facility">
                     <h3> Interested in having a yoga program at your facility?</h3>
@@ -47,9 +47,9 @@ get_header(); ?>
                     <?php endforeach; ?>
                     </ul>
                 </div>
-                <div class="button-case-holder"><button class="button-holder">Apply Now</button></div>
+                <div class="button-wrap"><button class="button-holder">Apply Now</button></div>
             </section>
-            <section class="volunteer-ops">
+            <section class="container-three">
                 <h2 class="h2-holder">Our Current Partners</h2>
                 <div class = "partners-container">
                     <h3>Current Facility Partners</h3>
--- themes/yoga_outreach/page-templates/support.php
+++ themes/yoga_outreach/page-templates/support.php
@@ -12,7 +12,7 @@ get_header(); ?>
             <header class="entry-header custom-hero-image">
                 <h1 class="header-title"><?php the_title(); ?></h1>
                 <p>We partners with yoga instructors, community organizations, social service agencies, and prisons to provide trauma-informed and strengths-based yoga programming for our community.</p>
-                <div class="button-wrap">
+                <div>
                     <button class="button-holder">Donate</button>
                     <button class="button-holder">Become Event Sponsor</button>
                     <button class="button-holder">Shop T-Shirt</button>
@@ -28,26 +28,26 @@ get_header(); ?>
             <section class="container-one">
                 <h2 class="h2-holder">Donate</h2>
                 <p>To make a donation today click HERE or give us a call to discuss your support at 604.385.3891 today. We gratefully acknowledge any contribution of $100 or more in our newsletter and via social media. “DONATE” leads to Canada Helps, and the number input going to show on Canada Helps input box.</p>
-                <div class="button-case-holder"><button class="button-holder">Apply Now</button></div>
+                <div class="button-wrap"><button class="button-holder">Apply Now</button></div>
             </section>
-            <section class="volunteer-community">
+            <section class="container-one">
                 <h2 class="h2-holder">Become a Monthly Contributor</h2>
                 <img/>
                 <p>For as little as $15 a month, less than a drop in yoga class, you can ensure that youth and adults in mental health facilities, prison or youth custody, alcohol and drug treatment centers, women and children who have experienced violence, or adults suffering from early onset dementia, have access to the powerful and healing practices of yoga and meditation, providing a method to reclaim their minds and bodies, to self regulate, and to connect with people in their community.</p>
                 <button class="button-holder">Donate Now</button>
             </section>
-            <section class="volunteer-advisor">
+            <section class="container-three">
                 <h2 class="h2-holder">Become an Event Sponsor</h2>
                 <p>Yoga Outreach hosts a number of events each year and you could be a sponsor. Our main events are the May Yoga Retreat and the October 30 Day Yoga Challenge. We are always looking for Corporate support to make these events as successful as possible. Your contribution will help us provide scholarships for Yoga Outreach students to attend, provide much needed publicity, and of course cover the cost of hosting an event! We provide varying levels of recognition based on your contribution. </p>
                 <h3>To become an event sponsor big or small please contact Delanie </h3>
-                <div><button class="button-holder">Contact</button></div>
+                <div class="button-wrap"><button class="button-holder">Contact</button></div>
             </section>
-            <section class="t-shirt-container">
+            <section class="container-tshirt">
                 <h2 class="h2-holder">Get a T-Shirt</h2>
                 <p>Our new t-shirts made by Fairware, these shirts are sustainably made, ethically sourced and absolutely gorgeous to wear! In a wide variety of colours and styles you are sure to find one you love and all proceeds support our programming.</p>
                 <button class="button-holder">Shop T-Shirts</button>
             </section>
-            <section class="volunteer-opportun">
+            <section class="container-one">
                 <h2 class="h2-holder">Our Current Sponsors</h2>
                 <p>Yoga Outreach's past 20 years of community service has only been possible thanks to volunteers and individuals like YOU who donate to support the healing of your community.</p>
                 <h3>Program Sponsors, In-kind & Financial Support</h3>
