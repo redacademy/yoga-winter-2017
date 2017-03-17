06732ab576bd330726c7ec9d9e1a1e4c594f5fa0
Edward <you@email.com>
2017-03-17 10:25:36 -0700

    added date field suite and fixed curly bracket

--- themes/yoga_outreach/page-templates/core-training.php
+++ themes/yoga_outreach/page-templates/core-training.php
@@ -22,19 +22,23 @@ get_header(); ?>
                 	<p class="core-header">Yoga Outreach Core Training</p>
 				</div><!--core-h3-container-->
 				<button class ="book-training-button">Register Online</button>
-				<p class ="small-para">- Yoga Alliance 24 Continuing Education Non-Contact hrs</p>
-				<p class ="small-para">- BCRPA Continuing Education Credits</p>
+					<p class ="small-para">- Yoga Alliance 24 Continuing Education Non-Contact hrs</p>
+					<p class ="small-para">- BCRPA Continuing Education Credits</p>
 				<div class ="avail-online-heading-container">
 					<img src ="<?php echo get_template_directory_uri(); ?>/images/earth_icon.svg" class = "earth-logo"alt ="earth_icon">
 					<h3 class ="avail-online-h3">available online and in-classroom format!</h3>
 				</div>
-				<p><?php echo CFS()->get('training_paragraph'); ?></p>
+				<p class = "light-training-para"><?php echo CFS()->get('light_paragraph'); ?></p>
+				<p class = "dark-training-para"><?php echo CFS()->get('dark_paragraph'); ?></p>
 			</section><!--section-15px-padding-->
 			<div class ="testimonal-container">
 			<!--empty div-->
 			</div>
-			<section class ="section-15px-padding">
-			<!--Add Calender-->
+			<section class ="section-15px-padding dates-section">
+			<div class ="icon-heading-container">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/earth_icon.svg" class = "earth-logo" alt = "earth_icon">
+				<h3><?php echo CFS()->get('training_date_header'); ?></h3>
+			</div>
 			</section>
 			<ul class = "training-list">
 				<li class="training-list-item">Learning Objectives</li>
