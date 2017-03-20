d05f1f2d79a29e1a0fa4480a435b2ae616a3e664
Montana Sawyer <montanarsawyer@gmail.com>
2017-03-20 12:44:16 -0700

    styled volunteer

--- themes/yoga_outreach/page-templates/volunteer.php
+++ themes/yoga_outreach/page-templates/volunteer.php
@@ -15,14 +15,14 @@ get_header(); ?>
                     <p>We partners with yoga instructors, community organizations, social service agencies, and prisons to provide trauma-informed and strengths-based yoga programming for our community.</p>
                 </div>
                 <div>
-                    <button class="button-holder"><a href="#instructor" class="button">Become a Yoga Teacher</a></button>
-                    <button class="button-holder"><a href="#volunteer" class="button">Community Volunteer</a></button>
-                    <button class="button-holder"><a href="#council" class="button">Advisory Council Member</a></button>
-                    <button class="button-holder"><a href="#opportunities" class="button">Reaching Out with Yoga</a></button>
+                    <button class="grey-button main-button"><a href="#instructor" class="button">Become a Yoga Teacher</a></button>
+                    <button class="grey-button main-button"><a href="#volunteer" class="button">Community Volunteer</a></button>
+                    <button class="grey-button main-button"><a href="#council" class="button">Advisory Council Member</a></button>
+                    <button class="grey-button main-button"><a href="#opportunities" class="button">Reaching Out with Yoga</a></button>
                 </div>
             </header><!-- .entry-header -->
             <section class="container-one">
-                <h2 class="h2-holder">Benefits for Volunteers</h2>
+                <h2>Benefits for Volunteers</h2>
                 <div class = "list-container">
                     <div class = "decorative-line-case"><span class="decorative-line"></span></div>
                     <ul>
@@ -39,7 +39,7 @@ get_header(); ?>
                     </div>
             </section>
             <section id="instructor" class="container-two">
-                <h2 class="h2-holder">Volunteer as a Yoga Instructor</h2>
+                <h2>Volunteer as a Yoga Instructor</h2>
                 <ol>
                 <?php
                     $fields = CFS()->get( 'volunteer_steps' );
@@ -53,22 +53,22 @@ get_header(); ?>
                         <?php endforeach; ?>
                     <?php endif; ?>
                 </ol>
-                <div class="button-wrap"><button class="button-holder">Apply Now</button></div>
+                <div class="button-wrap"><button class="small-button white-button"><a href="#opportunities" class="button">Apply Now</a></button></div>
             </section>
             <section id="volunteer" class="container-one">
-                <h2 class="h2-holder">Volunteer for Community Engagement</h2>
-                <p><?php echo CFS()->get( 'community_engagement' ); ?></p>
-                <button class="button-holder">Apply Now</button>
+                <h2>Volunteer for Community Engagement</h2>
+                <p class="info-paragraph"><?php echo CFS()->get( 'community_engagement' ); ?></p>
+                <button class="grey-button small-button"><a href="#opportunities" class="button">Apply Now</a></button>
             </section>
             <section id="council" class="container-three">
-                <h2 class="h2-holder">Volunteer for Advisory Council</h2>
-                <p><?php echo CFS()->get( 'advisory_council' ); ?></p>
-                <button class="button-holder">Contact Us</button>
+                <h2>Volunteer for Advisory Council</h2>
+                <p class="info-paragraph"><?php echo CFS()->get( 'advisory_council' ); ?></p>
+                <button class="general-button white-button"><a href="#opportunities" class="button">Contact Us</a></button>
             </section>
             <section id="opportunities" class="container-four">
-                <h2 class="h2-holder">Volunteer Opportunities</h2>
+                <h2>Volunteer Opportunities</h2>
                 <p></p>
-                <button class="button-holder">View Here</button>
+                <button class="general-button white-button"><a href="#opportunities" class="button">View Here</a></button>
             </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
