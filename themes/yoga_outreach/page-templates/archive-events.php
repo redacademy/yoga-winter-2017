d44033c29b6254a65336c7d3fab0847d8606b365
Warren Chan <warrenchan@Warrens-MacBook-Pro.local>
2017-03-21 14:04:18 -0700

    Event loop created marked up and styled

--- themes/yoga_outreach/page-templates/archive-events.php
+++ themes/yoga_outreach/page-templates/archive-events.php
@@ -24,60 +24,63 @@ get_header(); ?>
 					<button class="general-button teal-button">retreat</button>
 				</div>
 			</header><!--general-template-section-->
+
             <section class="container">
-                <h2>Event heading</h2>
-                <div class="body-info">
-                    <div class="title-contain">
-                        <h3>workshop title</h3>
-                        <div class="accent-name-shape"></div>
-                    </div>
-                    <div class="img-contain">
-                        <img src="" alt="workshopimg">
-                    </div>
+                <?php $events= CFS()->get( 'new_event' ); ?>  <!--variable to hold email from loop-->    
 
-                    <table>
-                        <tr>
-                            <td><h4>Date</h4></td>
-                            <td><p>Doe</p></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Location</h4></td>
-                            <td><p>Doe</p></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Price (CAD)</h4></td>
-                            <td><p>Doe</p></td>
-                        </tr>
-                    </table>
+                <?php if(!empty($events)): ?>
+                    <?php foreach( $events as $event ): ?>
+                        <h2><?php echo $event['event_header']; ?></h2>
 
-                    <article>
-                        <p>
-                            Most teacher trainings provide teachers with the essential foundations for conducting public classes but often leave graduates without more in-depth training in negotiating the teacher/student relationship . 
-                            Yoga teachers may lack an understanding community to address questions about their work, debrief difficult teacher/student relationships and discuss ethics. In this workshop participants will gain a theoretical and experiential foundation in the unique qualities of the teacher/student relationship as it pertains to yoga teaching in diverse settings, personal and professional boundaries, self care and what it means to teach from a strengths-based, trauma-informed place.
-                        </p>
-                    </article>
-                </div>
-                <div class="info-dropdown">
-                        <h3>About the facilitator</h3><span>+</span>
-                    </div>
-                    <div class="info-field">
-                        <?php $fields= CFS()->get( 'staff_members' ); ?>  <!--variable to hold staffs from loop-->
+                        <div class="body-info">
+                            <div class="title-contain">
+                                <h3><?php echo $event['event_title']; ?></h3>
+                                <div class="accent-name-shape"></div>
+                            </div>
 
-                        <?php if(!empty($fields)): ?>
-                            <?php foreach( $fields as $field ): ?>
-                                <div class="stafflist"> 
-                                    <img src="<?php echo $field['']; ?>" alt="staff photo" class="staffphoto">
-                                    <p><?php echo $field['']; ?></p>
-                                    <div class="accent-name-shape"></div>
-                                </div>
-                            <?php endforeach; ?>
-                        <?php endif; ?>  
-                    </div>
+                            <div class="img-contain">
+                                <img src="<?php echo $event['event_image'] ?>" alt="event-image" class="event-img">
+                            </div>
 
-                    <div class="booking-button">
-                        <button class="general-button white-button">book workshop</button>
-                    </div>
+                            <table>
+                                <tr>
+                                    <td><h4>Date</h4></td>
+                                    <td><p><?php echo $event['event_date']; ?></p></td>
+                                </tr>
+                                <tr>
+                                    <td><h4>Location</h4></td>
+                                    <td><p><?php echo $event['event_location']; ?></p></td>
+                                </tr>
+                                <tr>
+                                    <td><h4>Price (CAD)</h4></td>
+                                    <td><p><?php echo $event['event_price']; ?></p></td>
+                                </tr>
+                            </table>
+
+                            <article>
+                                <p><?php echo $event['event_info']; ?></p>
+                            </article>
+                        </div>
+
+                        <div class="info-dropdown">
+                            <h3>About the facilitator</h3><span>+</span>
+                        </div>
+                                
+                        <div class="info-field">
+                            <div class="facilitator">
+                                <img src="<?php echo $event['facilitator_image'] ?>" alt="event-image">
+                                <p><?php echo $event['facilitator_name']; ?></p>
+                                <div class="accent-name-shape"></div>
+                            </div>
+                            <p><?php echo $event['facilitator_bio']; ?></p>
+                        </div>
+                    <?php endforeach; ?>
+                <?php endif; ?> 
             </section>
+            <div class="booking-button">
+                <button class="general-button white-button">book workshop</button>
+            </div>
+
             
          <section class="container2">
                 <h2>Retreat heading</h2>
