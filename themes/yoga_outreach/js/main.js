a6d3a5d625d13977bc81eca250009650d98403fc
Edward <you@email.com>
2017-03-22 16:16:01 -0700

    finished styling training-main.php

--- themes/yoga_outreach/js/main.js
+++ themes/yoga_outreach/js/main.js
@@ -26,6 +26,7 @@
         }
     });
 
+    // Remove infoField Class
     function resize() {
         if ($window.width() > 720) {
             $infoField.removeClass('info-field');
@@ -48,13 +49,15 @@
         }, 500);
     });
 
+
+    //Click Read More dropdown
     $('.show-videos li:gt(2)').hide();
     $('.read-more').click(function() {
-    $('.show-videos li:gt(2)').show('2000','swing');
-    $('.read-less').click(function(){
-    $('.show-videos li:gt(2)').hide('2000','linear');
-    })
-});
+        $('.show-videos li:gt(2)').show('2000','swing');
+        $('.read-less').click(function(){
+            $('.show-videos li:gt(2)').hide('2000','linear');
+        })
+    });
 
     // Changes radio buttons on forms
     $( '.gfield_radio li label' ).prepend( '<span></span>');
