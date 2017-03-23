6ec3794bade6c6383e5a310f62126d6e7a9038c4
inhyechoi <inhyechoi0404@gmail.com>
2017-03-23 15:43:08 -0700

    deleted unnecessary footer code

--- themes/yoga_outreach/footer.php
+++ themes/yoga_outreach/footer.php
@@ -55,11 +55,6 @@
 <?php wp_footer(); ?>
 
 </div><!-- #content -->
-    <footer id="colophon" class="site-footer" role="contentinfo">
-      <div class="site-info">
-      </div><!-- .site-info -->
-    </footer><!-- #colophon -->
-
   </div><!-- #page -->
 
 	<?php wp_footer(); ?>
