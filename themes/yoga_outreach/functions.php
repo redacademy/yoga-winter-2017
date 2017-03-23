238e5cc21d8af64e7cd4ac3adb117a8da59ec163
inhyechoi <inhyechoi0404@gmail.com>
2017-03-23 15:38:02 -0700

    fixed enque script code

--- themes/yoga_outreach/functions.php
+++ themes/yoga_outreach/functions.php
@@ -96,6 +96,8 @@ function red_starter_scripts() {
 
 	wp_enqueue_script( 'yoga-outreach-training-modules', get_template_directory_uri() . '/build/js/training-modules.min.js', array('jquery'), '20130115', true );
 
+  wp_enqueue_script( 'main', get_template_directory_uri() . '/build/js/main.min.js', array( 'jquery' ), '1.0.0', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -112,10 +114,3 @@ require get_template_directory() . '/inc/template-tags.php';
  */
 require get_template_directory() . '/inc/extras.php';
 
-/**
-* Including jquery in javascript
-*/
-function my_theme_scripts() {
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/build/js/main.min.js', array( 'jquery' ), '1.0.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
