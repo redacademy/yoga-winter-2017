4ed8a42a6696de3656edbe2889101bf669e8b77d
inhyechoi <inhyechoi0404@gmail.com>
2017-03-28 14:55:48 -0700

    cleaned code

--- themes/yoga_outreach/header.php
+++ themes/yoga_outreach/header.php
@@ -12,13 +12,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-	<?php wp_head(); ?>
+	  <?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
-
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text">
@@ -27,58 +25,17 @@
           </h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
-
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					   <div id="header-logo">
+          <div id="header-logo">
             <div class="logo-image">
               <?php show_easylogo(); ?>
             </div>
-					</div>
+          </div><!-- .header-logo -->
           <span class="responsive-menu">
 				    <i class="fa fa-bars" aria-hidden="true"></i>
 		    	</span>
-            <div class="menu-dropdown">
-              <div class="wrapper">
-                <form class="form-signin">       
-                  <h3>username</h3>
-                  <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-                  <h3>password</h3>
-                  <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
-                  <label class="checkbox">
-                    <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
-                  </label>
-                  <button class="btn btn-lg btn-primary btn-block dropdown-btn" type="submit">
-                    <a href="">sign in</a>
-                  </button>   
-                </form>
-              </div>
-            </div>
-           <div class="show-dropdown show">
-             <div class="wrapper">
-                <form class="form-signin">       
-                  <h3>username</h3>
-                  <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-                  <h3>password</h3>
-                  <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
-                  <label class="checkbox">
-                    <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
-                  </label>
-                  <button class="btn btn-lg btn-primary btn-block dropdown-btn" type="submit">
-                    <a href="">sign in</a>
-                  </button>   
-                </form>
-              </div>
-              <ul class="dropdown-menu-list">
-              <?php get_search_form();?>
-              </ul>
-              <?php wp_nav_menu( 
-                array( 'theme_location' => 'primary', 
-                      'menu_id' => 'primary-menu') ); 
-              ?>
-            </div>
-
-            <div class="signin-section">
-              <div class="wrapper">
+          <div class="menu-dropdown">
+            <div class="wrapper">
               <form class="form-signin">       
                 <h3>username</h3>
                 <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
@@ -87,19 +44,58 @@
                 <label class="checkbox">
                   <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
                 </label>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                <button class="btn btn-lg btn-primary btn-block dropdown-btn" type="submit">
                   <a href="">sign in</a>
                 </button>   
               </form>
-            </div>
-            <button class="signin-button"><a href="">sign in
-            </a></button>
-            </div>
-            <div class="mainmenu-list">
-              <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu') );?>
+            </div> <!-- .wrapper -->
+          </div><!-- .menu-dropdown -->
+          <div class="show-dropdown show">
+            <div class="wrapper">
+              <form class="form-signin">       
+                <h3>username</h3>
+                <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+                <h3>password</h3>
+                <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+                <label class="checkbox">
+                  <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+                </label>
+                <button class="btn btn-lg btn-primary btn-block dropdown-btn" type="submit">
+                  <a href="">sign in</a>
+                </button>   
+              </form>
+            </div><!-- .wrapper -->
+            <ul class="dropdown-menu-list">
               <?php get_search_form();?>
-            </div>
+            </ul>
+            <?php wp_nav_menu( 
+              array( 'theme_location' => 'primary', 
+                    'menu_id' => 'primary-menu') ); 
+            ?>
+          </div><!-- .show-dropdown .show -->
+          <div class="signin-section">
+            <div class="wrapper">
+            <form class="form-signin">       
+              <h3>username</h3>
+              <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+              <h3>password</h3>
+              <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+              <label class="checkbox">
+                <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+              </label>
+              <button class="btn btn-lg btn-primary btn-block" type="submit">
+                <a href="">sign in</a>
+              </button>   
+            </form>
+          </div><!-- .wrapper -->
+          <button class="signin-button">
+            <a href="">sign in</a>
+          </button>
+          </div><!-- .signin-section -->
+          <div class="mainmenu-list">
+            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu') );?>
+            <?php get_search_form();?>
+          </div>
         </nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
-
-			<div id="content" class="site-content">
+    <div id="content" class="site-content">
