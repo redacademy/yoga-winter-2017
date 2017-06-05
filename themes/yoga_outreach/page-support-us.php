4256b581a15c6c672b6cab84695326f58ee5abbb
Montana Sawyer <montanarsawyer@gmail.com>
2017-06-05 16:29:44 -0700

    fix issues

--- themes/yoga_outreach/page-support-us.php
+++ themes/yoga_outreach/page-support-us.php
@@ -4,7 +4,7 @@
  *
  * @package Yoga_Outreach_Theme
  */
- 
+
 get_header(); ?>
 
 	<div id="primary" class="content-area support-us-content">
@@ -16,9 +16,9 @@ get_header(); ?>
                 </div>
                 <div class="main-button-holder">
                     <a href="#donate" class="main-button clear-button">Donate</a>
-                    <a href="#sponsor" class="main-button clear-button">Become Event Sponsor</a>
+                    <a href="#sponsor" class="main-button clear-button">Become a Sponsor</a>
                     <a href="#tshirt" class="main-button clear-button">Shop T-Shirts</a>
-                    <a href="#current-sponsor" class="main-button clear-button">Current Sponsor</a>
+                    <a href="#current-sponsor" class="main-button clear-button">Current Sponsors</a>
                 </div>
             </header><!-- .entry-header -->
             <section class="container-three">
@@ -32,33 +32,39 @@ get_header(); ?>
                         </div>
                     <?php endforeach; ?>
                 </div>
-            </section> 
+            </section>
             <section id="donate" class="container-one">
                 <h2 class="h2-holder">Donate</h2>
                 <p><?php echo esc_html(CFS()->get( 'donation_information' )); ?></p>
                 <div class= "decorative-line2"></div>
-                <a href="<?php echo esc_url(CFS()->get( 'donation_page_url' ));?>" class="main-button dark-button">Donate Now</a>
+                <a href="<?php echo esc_url(CFS()->get( 'donation_page_url' ));?>" target="_blank" class="main-button dark-button">Donate Now</a>
             </section>
-            <section class="container-one">
+            <section class="container-one container-blue">
                 <div class="accent-background monthly"><div class="background-accent"></div></div>
                 <h2 class="h2-holder">Become a Monthly Contributor</h2>
                     <div class="contributor">
                         <img src="<?php echo esc_html(CFS()->get( 'monthly_contributor_image' )); ?>" alt="">
-                    </div> 
+                    </div>
                 <p><?php echo CFS()->get( 'monthly_contributor_info' ); ?></p>
-                <div><a href="<?php echo esc_url(CFS()->get( 'donation_page_url' ));?>" class="main-button dark-button">Donate Now</a></div>
+                <div><a href="<?php echo esc_url(CFS()->get( 'donation_page_url' ));?>" target="_blank" class="main-button white-button">Donate Now</a></div>
             </section>
             <section id="sponsor" class="container-one">
                 <div class="accent-background"><div class="background-accent"></div></div>
-                <h2 class="h2-holder">Become an Event Sponsor</h2>
+                <h2 class="h2-holder">Become a Sponsor</h2>
                 <p><?php echo esc_html(CFS()->get( 'event_sponsor' )); ?></p>
-                <h3>To become an event sponsor big or small please contact Delanie </h3>
-                <div class="button-wrap"><a href="<?php echo get_page_link(67); ?>" class="small-button white-button">Contact</a></div>
+                <div class="button-wrap">
+                    <h3>To become an event sponsor big or small please contact Delanie </h3>
+                    <a href="<?php echo get_page_link(67); ?>" class="small-button white-button">Contact</a>
+                </div>
             </section>
-            <section id="tshirt" class="container-tshirt">
+            <section class="container-one container-blue">
+                <div class="accent-background monthly"><div class="background-accent"></div></div>
                 <h2 class="h2-holder">Get a T-Shirt</h2>
+                    <div class="contributor">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/tshirt_mobile.png" alt="tshirt">
+                    </div>
                 <p><?php echo CFS()->get( 't_shirt_about' ); ?></p>
-                <div><a href="<?php echo esc_url(CFS()->get( 't_shirt_shop_url' ));?>" class="main-button white-button">Shop T-Shirts</a></div>
+                <div><a href="<?php echo esc_url(CFS()->get( 't_shirt_shop_url' ));?>" target="_blank" class="main-button white-button">Shop T-Shirts</a></div>
             </section>
             <section id="current-sponsor" class="container-one partners-container">
                 <h2 class="h2-holder">Our Current Sponsors</h2>
