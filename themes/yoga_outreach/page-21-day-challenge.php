3fbe5d9523d974f0f179c7b2e731f110ffc175cf
Montana Sawyer <montanarsawyer@gmail.com>
2017-08-09 09:51:57 -0700

    Add challenge page

---
+++ themes/yoga_outreach/page-21-day-challenge.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ *
+ *
+ * @package Yoga_Outreach_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area volunteer-content">
+		<main id="main" class="site-main" role="main">
+            <header class="entry-header custom-hero-image"></header><!-- .entry-header -->
+            <section class="page-main-header">
+                <div>
+                    <h1 class="header-title"><?php the_title(); ?></h1>
+                    <div class="info-paragraph info-p-center"><?php echo CFS()->get( 'about_challenge' ); ?></div>
+                </div>
+                <div class="button-container">
+                    <a href="#instructor" class="general-button grey-button">Teacher</a>
+                    <a href="#volunteer" class="general-button grey-button">Community Engagment</a>
+                    <a href="#council" class="general-button grey-button">Advisory Council</a>
+                    <a href="#opportunities" class="general-button grey-button">Projects</a>
+                </div>
+            </section>
+            <section id="instructor" class="container-two">
+                <h2>Volunteer as a Yoga Instructor</h2>
+                <ol>
+                <?php
+                    $fields = CFS()->get( 'challenge_steps' );
+                    if(!empty($fields)):
+                        foreach ( $fields as $field ):?>
+                            <div class="volunteer-steps">
+                                <li>
+                                <p class="volunteer-step"><?php echo $field['challenge_step']; ?></p>
+                                </li>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </ol>
+                <a href="volunteer-application-form" class="dark-button main-button apply-button">Apply Now</a>
+            </section>
+            <section id="volunteer" class="container-one">
+                <h2>Community Engagement Volunteer</h2>
+                <div class="info-paragraph"><?php echo CFS()->get( 'community_engagement' ); ?></div>
+                <a href="non-teaching-volunteer-application-form" class="grey-button small-button">Apply Now</a>
+            </section>
+            <section id="council" class="container-three">
+                <h2>Advisory Council Member</h2>
+                <div class="info-paragraph"><?php echo CFS()->get( 'advisory_council' ); ?></div>
+                <a href="non-teaching-volunteer-application-form" class="main-button white-button">Apply Now</a>
+            </section>
+            <section class="container-one">
+                <h2>Benefits for Volunteers</h2>
+                <div class = "list-container">
+                    <ul>
+                        <?php
+                        $fields = CFS()->get( 'volunteer_benefits' ); // returns an array of posts
+                        if(!empty($fields)):
+                            foreach ( $fields as $field ):?>
+                                <li>
+                                    <p><?php echo esc_html($field['volunteer_benefit']); ?></p>
+                                </li>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </ul>
+                    </div>
+            </section>
+            <section id="opportunities" class="container-four">
+                <h2>Volunteer Opportunities</h2>
+                <p></p>
+                <a href="current-opportunities" class="white-button main-button">View Here</a>
+            </section>
+
+            <?php if(!empty(CFS()->get( 'our_sponsors' ))): ?>
+                <section id="current-sponsor" class="container-one partners-container">
+                <h2 class="h2-holder">Our Sponsors</h2>
+                    <div class="info-field custom-info-container"><ul>
+                        <?php
+                        $fields = CFS()->get( 'our_sponsors' ); // returns an array of posts
+                            foreach ( $fields as $field ):?>
+                                <div>
+                                    <li>
+                                    <div class="info-paragraph"><?php echo $field['sponsor']; ?></div>
+                                    </li>
+                                </div>
+                    <?php endforeach; ?>
+                    </ul></div>
+            </section>
+            <?php endif; ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
