db120345a8b544f38678b990b8dcaccbfe6e6db7
Montana Sawyer <montanarsawyer@gmail.com>
2017-09-08 13:38:28 -0700

    update media page

--- themes/yoga_outreach/page-resources-media.php
+++ themes/yoga_outreach/page-resources-media.php
@@ -37,62 +37,67 @@ get_header(); ?>
 				</ul>
 			</section>
 			<div class="main-h3-container section-15px-padding">
-                <h3>Media & Video</h3>
-                <p>For Inquiries, email communications@yogaoutreach.com</p>
-            </div><!--main-h3-container-->
+					<h3>Yoga Outreach News</h3>
+			</div><!--main-h3-container-->
 			<section class="media-article-section">
 				<div class="section-15px-padding">
-				<?php if(!empty(CFS()->get( 'in_the_media' ))): ?>
+				<?php if(!empty(CFS()->get( 'in_the_press' ))): ?>
 					<div class="info-dropdown media-heading-flex">
-						<h3>In the Media</h3>
-						<span>+</span>
-							<p class="research-paper-para">Research papers & interesting articles about trauma-sensitive yoga.</p>
-					</div><!--info-dropdown-->
-
-					<div class="info-field">
-						<ul class="media-article-list show-videos">
-						<?php
-						$medias = CFS()->get('in_the_media');
-						foreach ( $medias as $media ):
-						?>
-						<a href="<?php echo esc_html($media['media_link']); ?>">
-							<li class="media-item">
-								<h4 class="media-date"><?php echo esc_html($media ['date']);?>
-									<div class="media-color media-blue"></div>
-								</h4>
-								<p><?php echo esc_html($media['content']);?></p>
-							</li>
-						</a>
-						<?php endforeach ?>
-						</ul><!--media-article-list-->
-					</div><!--info-field-->
-					<?php endif; ?>
-
-					<div class="info-dropdown">
-						<h3>In the Press</h3>
+						<h3>News</h3>
 						<span>+</span>
 					</div><!--info-dropdown-->
 					<div class="info-field">
 						<ul class="press-article-list">
 						<?php
-						$articles = CFS()->get('in_the_press');
-
-						foreach ( $articles as $article ):
+						$pdfs = CFS()->get('in_the_press');
+						foreach ( $pdfs as $pdf ):
 						?>
-						<p class="research-paper-para">Research papers & interesting articles about trauma-sensitive yoga.</p>
-						<a href="<?php echo esc_html($article ['read_article_link']) ?>">
-							<li class="press-item">
-								<h4 class="media-date"><?php echo $article ['date'];?>
-									<div class="media-color media-purple"></div>
-								</h4>
-								<p><?php echo esc_html($article ['content']);?></p>
-							</li>
-						</a>
+						<a href="<?php echo esc_html($pdf ['pdf_item']);?>" >
+						<li class="media-item">
+							<h4 class="media-date"><?php echo esc_html($pdf ['pdf_date']);?>-<?php echo esc_html($pdf['name_of_pdf']);?>
+								<div class="media-color media-purple"></div>
+							</h4>
+							<p><?php echo esc_html($pdf['pdf_description']);?></p>
+						</li>
+					</a>
 						<?php endforeach ?>
 						</ul><!--media-article-list-->
 					</div><!--info-field-->
+					<?php endif; ?>
 				</div>
 			</section><!--media-article-section-->
+
+			<div class="main-h3-container section-15px-padding">
+				<h3>Yoga Outreach in the Media</h3>
+			</div><!--main-h3-container-->
+			<section class="media-article-section">
+			<div class="section-15px-padding">
+			<?php if(!empty(CFS()->get( 'in_the_media' ))): ?>
+				<div class="info-dropdown media-heading-flex">
+					<h3>Media</h3>
+					<span>+</span>
+				</div><!--info-dropdown-->
+
+				<div class="info-field">
+					<ul class="media-article-list show-videos">
+					<?php
+					$medias = CFS()->get('in_the_media');
+					foreach ( $medias as $media ):
+					?>
+					<a href="<?php echo esc_html($media['media_link']); ?>">
+						<li class="media-item">
+							<h4 class="media-date"><?php echo esc_html($media ['date']);?>
+								<div class="media-color media-blue"></div>
+							</h4>
+							<p><?php echo esc_html($media['content']);?></p>
+						</li>
+					</a>
+					<?php endforeach ?>
+					</ul><!--media-article-list-->
+				</div><!--info-field-->
+			<?php endif; ?>
+			</div>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
